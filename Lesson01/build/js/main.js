66d0d954dec92ebd5ff52fb903fe4dff71260c79
Saswat Samal <saswat1091@outlook.com>
2023-05-11 11:59:46 +0530

    Lesson 1 added

--- Lesson01/build/js/main.js
+++ Lesson01/build/js/main.js
@@ -1,17 +1,11 @@
-let username = 'Saswat'
-console.log(username)
-
+"use strict";
+let username = 'Saswat';
+console.log(username);
 // let a = 12
 // let b = '6'
 // let c = 2
-
-
 // console.log(a/b) this will not be problem in js file but typescript shows an error
 // The right-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type.ts(2363)
-
-
-let a : number= 12
-let b : number = 6
-let c : number = 2
-
-
+let a = 12;
+let b = 6;
+let c = 2;
