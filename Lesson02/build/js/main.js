204c68c2a867a71fcae9da88f55aad896f71fdea
Saswat Samal <saswat1091@outlook.com>
2023-05-11 15:13:23 +0530

    Lesson 2 added

---
+++ Lesson02/build/js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let myName = 'Dave';
+let anyType; // defines a data type of type any
+let album; // union data type
+let re = /\w+/g;
---
+++ Lesson02/src/main.ts
@@ -0,0 +1,6 @@
+let myName: string = 'Dave'
+
+let anyType: any // defines a data type of type any
+
+let album: string | number // union data type
+let re: RegExp = /\w+/g
